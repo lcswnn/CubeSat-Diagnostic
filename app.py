4b39c5b2fadc3215928f66f053e7b7a0dc82efd5
Lucas Waunn <lucaswaunn@gmail.com>
2026-04-02 14:45:17 -0500

    Adding CubeSat dataset as well as function to upload files.

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st 
+import pandas as pd
+
 st.title("Hello world!")
 st.write("This is the start of the CubeSat Diagnostic app.")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
+    df = pd.read_csv(uploaded_file)
+    st.write("Here's a preview of the data:")
+    st.dataframe(df.head())
+else:    
+  st.write("Please upload a file to proceed.")
